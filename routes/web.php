fa7fccfa3cdb93223b986d968ad228325b076d84
Manfred Parbatia <superflyman90@gmail.com>
2021-01-06 21:48:59 +0100

    modification du fichier web et ajout fichier html de test

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::any('/test', function () {
+    return 'GET route';
+});
+
+Route::get('/article/{id}', function ($id) {
+    return $id;
+});
